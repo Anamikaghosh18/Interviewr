59db0033674fd683cb30b9b9819b081f0854a315
Anamikaghosh18 <g.anamika0101@gmail.com>
2026-03-12 03:34:22 +0530

    Auth-DB sync

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,44 @@
+import User from "../models/User.js";
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+
+export const inngest = new Inngest({ id: "Interviewr" });
+
+const syncUser = inngest.createFunction(
+  {
+    id: "sync-user",
+  },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+    const { id, email_addresses, first_name, last_name, image_url } =
+      event.data;
+
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_addresses,
+      name: `${first_name || ""}${last_name}`,
+      profile_img: image_url,
+    };
+
+    await User.create(newUser);
+
+    // to-do sth else
+  },
+);
+
+const deleteUser = inngest.createFunction(
+  {
+    id: "delete-user",
+  },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+
+    // to-do sth else
+  },
+);
+
+export const functions = [syncUser, deleteUser];
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,25 +2,34 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import serve from "inngest";
+import { inngest } from "./lib/inngest.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
-app.get("/api", (req, res) => {
-  res.status(200).json({ msg: "Success from server" });
-});
+// middlewares
+app.use(express.json());
 
+// server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.use("api/inngest", serve);
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "API is up and running" });
+});
 
 const startServer = async () => {
   try {
-    await connectDB(); 
+    await connectDB();
     app.listen(ENV.PORT, () => {
       console.log("🌐 Server is running on port:", ENV.PORT);
     });
   } catch (error) {
-    console.log("❌ Error starting the server", error); 
+    console.log("❌ Error starting the server", error);
   }
 };
 
-startServer(); 
+startServer();
