066364d0117f55d1fd4906aa785a7e9ff701e96f
Anamikaghosh18 <g.anamika0101@gmail.com>
2026-03-12 01:59:17 +0530

    DB and external tools setup with Backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
@@ -10,15 +11,16 @@ app.get("/api", (req, res) => {
   res.status(200).json({ msg: "Success from server" });
 });
 
-// make ready for prod
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+const startServer = async () => {
+  try {
+    await connectDB(); 
+    app.listen(ENV.PORT, () => {
+      console.log("🌐 Server is running on port:", ENV.PORT);
+    });
+  } catch (error) {
+    console.log("❌ Error starting the server", error); 
+  }
+};
 
-app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
-
-export default app;
+startServer(); 
